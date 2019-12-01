86cd8950c1681fe8c1753201b0d098d19d1ef5a2
swandenson <58196208+swandenson@users.noreply.github.com>
2019-12-01 18:58:22 -0500

    Update day1.py

--- day1/day1.py
+++ day1/day1.py
@@ -7,9 +7,21 @@ modules = myfile.read()
 
 modules = modules.split("\n")
 modules = [int(i) for i in modules]
-print(modules)
 
-for i in range(len(modules)):
-	modules[i] = ((modules[i] // 3) - 2)
+def calc_fuel(mass):
+	for i in range(len(modules)):
+		modules[i] = ((modules[i] // 3) - 2)
+	return sum(modules)	
 
-print(sum(modules))
+print(calc_fuel(modules))
+
+def total_fuel(data):
+	totalFuel = 0
+	for i in range(len(modules)):
+		add_fuel = calc_fuel(modules[i])
+		if add_fuel <= 0:
+			break
+		totalFuel += add_fuel
+	return totalFuel
+
+print(total_fuel(modules))
